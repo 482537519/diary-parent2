20efe409f6e22dc3331a1406eed3364a77c43ea6
482537519 <55521729+482537519@users.noreply.github.com>
2020-08-28 10:07:46 +0800

    Update UploadUtil.java

--- manager-diary/src/main/java/com/kmu/manager/util/UploadUtil.java
+++ manager-diary/src/main/java/com/kmu/manager/util/UploadUtil.java
@@ -25,8 +25,8 @@ public class UploadUtil {
             // Endpoint以杭州为例，其它Region请按实际情况填写。
             String endpoint = "https://oss-cn-chengdu.aliyuncs.com";
             // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-            String accessKeyId = "LTAI4GGUFTeM9yeJB9oN3AQj";
-            String accessKeySecret = "Tpso9BlL4fUe802zAQ2Q97FtmHJ4Dl";
+            String accessKeyId = "";
+            String accessKeySecret = "";
             // 创建OSSClient实例。
             OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
             PutObjectResult putObjectResult = ossClient.putObject(bucketName, fileName, inputStream);
